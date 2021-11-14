92e499f91d592419eaf109ab0c9691c224e1c8d0
EC2 Default User <ec2-user@ip-172-31-2-210.ec2.internal>
2021-11-14 05:18:23 +0000

    [add]cartカラム

--- db/migrate/20211114025810_create_carts.rb
+++ db/migrate/20211114025810_create_carts.rb
@@ -1,7 +1,12 @@
 class CreateCarts < ActiveRecord::Migration[5.0]
   def change
     create_table :carts do |t|
-
+      t.integer :item_id ,null:false
+      t.integer :user_id ,null:false
+      t.integer :user_id ,null:false
+      t.integer :quantity ,null:false
+      
+      
       t.timestamps
     end
   end
