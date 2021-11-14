3f8f98b2334bd3c4a7a254ae2ac87833eac87a24
kkjos <k19951227@gmail.com>
2021-11-14 05:24:47 +0000

    ADDorder_coloum

--- db/migrate/20211114025822_create_orders.rb
+++ db/migrate/20211114025822_create_orders.rb
@@ -1,7 +1,15 @@
 class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
-
+      
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.string :orderer_name, null: false
+      t.boolean :payment_way, null: false, default: 0
+      t.integer :order_status, null: false, default: 0
+      t.integer :postage, null: false
+      t.integer :claimed, null: false
+      
       t.timestamps
     end
   end
