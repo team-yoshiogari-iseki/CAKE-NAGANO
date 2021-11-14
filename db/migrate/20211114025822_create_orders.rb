ab4fae628df9d0e8c377c86d9085b0d4fc2df936
kkjos <k19951227@gmail.com>
2021-11-14 05:47:38 +0000

    [fix]order_column

--- db/migrate/20211114025822_create_orders.rb
+++ db/migrate/20211114025822_create_orders.rb
@@ -2,11 +2,11 @@ class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
       
-      t.string :postal_code, null: false
+      t.integer :postal_code, null: false
       t.string :address, null: false
       t.string :orderer_name, null: false
-      t.boolean :payment_way, null: false, default: 0
-      t.integer :order_status, null: false, default: 0
+      t.integer :payment_way, null: false, default: 0
+      t.string :order_status, null: false, default: 0
       t.integer :postage, null: false
       t.integer :claimed, null: false
       
