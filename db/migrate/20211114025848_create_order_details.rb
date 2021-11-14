2638aaef89bc4679d9b4959874d6c0a8a1778e4b
EC2 Default User <ec2-user@ip-172-31-59-232.ec2.internal>
2021-11-14 06:42:40 +0000

    fix_column

--- db/migrate/20211114025848_create_order_details.rb
+++ db/migrate/20211114025848_create_order_details.rb
@@ -6,7 +6,7 @@ class CreateOrderDetails < ActiveRecord::Migration[5.0]
       t.integer :item_id
       t.integer :price, nill:false
       t.integer :quantity, nill:false
-      t.string :making_status, nill:false
+      t.integer :making_status, nill:false
       
       t.timestamps
     end
