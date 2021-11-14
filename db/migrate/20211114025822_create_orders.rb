cdb16810f6f215cc19dbaa3b35584bad12b9f072
kkjos <k19951227@gmail.com>
2021-11-14 09:28:33 +0000

    [fix]order_column

--- db/migrate/20211114025822_create_orders.rb
+++ db/migrate/20211114025822_create_orders.rb
@@ -2,6 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
       
+      t.integer :user_id
       t.integer :postal_code, null: false
       t.string :address, null: false
       t.string :orderer_name, null: false
