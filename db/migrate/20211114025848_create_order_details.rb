1b9cd5079c0a96748ca8673ac9c700754948e97c
EC2 Default User <ec2-user@ip-172-31-59-232.ec2.internal>
2021-11-14 06:09:09 +0000

    [add column&model]

--- db/migrate/20211114025848_create_order_details.rb
+++ db/migrate/20211114025848_create_order_details.rb
@@ -1,7 +1,13 @@
 class CreateOrderDetails < ActiveRecord::Migration[5.0]
   def change
     create_table :order_details do |t|
-
+      
+      t.integer :order_id
+      t.integer :item_id
+      t.integer :price, nill:false
+      t.integer :quantity, nill:false
+      t.string :making_status, nill:false
+      
       t.timestamps
     end
   end
