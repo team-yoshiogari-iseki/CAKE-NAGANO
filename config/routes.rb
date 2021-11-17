8929d3b9a0c9b49e7aeb050eb993092dd207583e
akiraeguchi <oneok.enjoymylife@gmail.com>
2021-11-17 14:50:53 +0000

    cart_items index 一旦終了

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,11 @@ Rails.application.routes.draw do
 
   scope module: :public do
     resources :addresses, except: [:new, :show]
-    resources :cart_items, except: [:new, :show, :edit]
+    resources :cart_items, except: [:new, :show, :edit] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
     resources :items, only: [:index, :show]
     resources :orders, except: [:edit, :update, :destroy]
     get 'orders/check' => 'orders#check'
