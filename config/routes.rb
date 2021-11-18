30e3f055aee6abadbbdfe3beccafae6fb48b0fe1
EC2 Default User <ec2-user@ip-172-31-2-210.ec2.internal>
2021-11-18 04:43:20 +0000

    [start]customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-
-
   devise_for :customer,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -17,16 +15,18 @@ Rails.application.routes.draw do
     resources :customers, except: [:new, :create, :destroy]
     resources :orders, only: [:index, :show, :update]
   end
-  
+    
    scope module: :public do
+    root to: 'homes#top'
     resources :addresses, except: [:new, :show]
     resources :cart_items, except: [:new, :show, :edit]
     resources :items, only: [:index, :show]
     resources :orders, except: [:edit, :update, :destroy]
     get 'orders/check' => 'orders#check'
     get 'orders/success' => 'orders/success'
-    resources :customers, only: [:show, :edit, :update]
-    get 'customers/erasure' => 'customers#erasure'
+    resources :customers, only: [:show, :edit,:update] do
+    member do get :erasure
+    end end
     delete 'customers/:id' => 'customers#leave'
    end
   end
