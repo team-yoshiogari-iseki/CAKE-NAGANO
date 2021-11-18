532e747a8ffbe72853bcfe5c9d28170258122695
kkjos <k19951227@gmail.com>
2021-11-18 13:28:18 +0000

    （未）showページ・日本語対応

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,8 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
+  
+  enum making_status: { not_startable:0, waiting_for_production:1, production:2, production_completed:3}
+  
 
 end
