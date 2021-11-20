c40d4dee043d589dc8697771f1d176b3ae5db3d9
MAO TORIHATA <4321namap@gmail.com>
2021-11-20 13:51:29 +0000

    配送先仮作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,45 @@
 class Public::AddressesController < ApplicationController
+
+  def index
+    @addresses = Address.all
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if@address.save
+      flash[:notice] = "配送先を登録しました。"
+      redirect_to addresses_path
+    else
+      render :index
+    end
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if@address.update
+      flash[:notice] = "変更を保存しました。"
+      redirect_to addresses_path
+    else
+      flash.now[:alert]
+      render :edit
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :address, :postal_code)
+  end
+
 end
