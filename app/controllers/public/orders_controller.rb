67327b8a588d9f2fd7f77acffe1a446adf2a46a0
akiraeguchi <oneok.enjoymylife@gmail.com>
2021-11-21 07:33:05 +0000

    order controller view 途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,22 +8,25 @@ class Public::OrdersController < ApplicationController
   def check
     @order = Order.new(order_params)
     @cart_items = current_customer.cart_items
-    @order.payment_way = params[:order][:payment_way]
+    @order.payment_way = params[@order][:payment_way]
+    @current_customer = current_customer
 
-    if params[:order][:address_option] == "0"
+    if params[@order][:address_option] == "0"
       @order.postal_code = current_customer.postal_code
       @order.order_address = current_customer.address
+      @order.orderer_name = current_customer.last_name + current_customer.first_name
 
-    elsif params[:order][:address_option] == "1"
+    elsif params[@order][:address_option] == "1"
       @order.order_address = params[:order][:order_address].to_i
       @order_address = Address.find
       @order.postal_code = @order_address.postal_code
       @order.order_address = @order_address.address
       @order.orderer_name = @order_address.orderer_name
 
-    elsif params[order][:address_option] == "2"
+    elsif params[@order][:address_option] == "2"
       @order.postal_code = params[:order][:postal_code]
       @order.order_address = params[:order][:order_address]
+      @order.order_name
     end
   end
 
