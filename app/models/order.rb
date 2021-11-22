6052a8fdbb028bf1e7c6a7ed9d64b37626d97239
kkjos <k19951227@gmail.com>
2021-11-22 03:08:18 +0000

    エラーだらけ

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
+  
+  enum order_status: { waiting_for_deposit:0, payment_confirmation:1, production:2, ready_to_ship:3, shipped:4}
 
 end
